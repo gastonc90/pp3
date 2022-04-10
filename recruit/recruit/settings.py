32019e877571b3465c9df02a5b80054fad7fb497
pelo <42307568+gastonc90@users.noreply.github.com>
2022-04-10 18:01:57 -0300

    Update settings.py

--- recruit/recruit/settings.py
+++ recruit/recruit/settings.py
@@ -131,7 +131,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'toolrecruit@gmail.com'
-EMAIL_HOST_PASSWORD = 'recruitertool4a!!'
+EMAIL_HOST_PASSWORD = ''
 
 
 
